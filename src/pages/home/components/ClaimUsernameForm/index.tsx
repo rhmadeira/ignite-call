5a6ff64e9cc8786840123982c364d459ede66d23
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-03-31 23:02:11 -0300

    feat: schema and react hook form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,30 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Form } from "./styles";
 
+const schemaClaimUsernameForm = z.object({
+  username: z.string(),
+});
+
+type IClaimUsernameFormData = z.infer<typeof schemaClaimUsernameForm>;
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<IClaimUsernameFormData>();
+
+  async function handleClaimUsername(data: IClaimUsernameFormData) {
+    console.log(data);
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register("username")}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
