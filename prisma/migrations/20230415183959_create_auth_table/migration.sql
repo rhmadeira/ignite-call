70dd58917ac902fdddeabc87e722f699c8254280
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-04-15 16:41:58 -0300

    feat: creating methods Adapter

---
+++ prisma/migrations/20230415183959_create_auth_table/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `Id` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - The required column `id` was added to the `users` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_users" ("name", "username") SELECT "name", "username" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
